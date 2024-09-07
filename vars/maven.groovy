1f6115244fc5f5c1bbb27b49839e2643fe0824c3
Amlan Das <dasamlan271988@gmail.com>
2024-09-07 22:45:49 +0530

    jkshared

---
+++ vars/maven.groovy
@@ -0,0 +1,31 @@
+def lintchecks(){
+        sh "whoami"
+        sh "echo *** Lint check starting for ${component} ***"
+        sh "mvn checkstyle:check || true"
+        sh "echo *** Lint check completed for ${component} ***"
+}
+
+def call(){
+        pipeline{
+                agent{
+                        label "node"
+                }
+                stages{
+                    stage("lintchecks"){
+                        steps{
+                                script {
+                                     lintchecks()           
+                                       }
+                                }
+                
+                        }
+                    stage("Static code analysis"){
+                        steps{
+                                sh "echo static code analysis in process"
+                        }
+                        
+                    }    
+                }
+
+}
+}               
