4a3d850e4de166b089ede43f1b31aedfa2126d63
Amlan Das <dasamlan271988@gmail.com>
2024-09-07 21:55:34 +0530

    jkshared

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,7 +1,7 @@
 def lintchecks(){
         sh "whoami"
         sh "echo *** Lint check starting for ${component} ***"
-        sh "npm i jslint"
+        sh "npm install jslint"
         sh "/home/centos/node_modules/jslint/bin/jslint.js server.js || true"
         sh "echo *** Lint check completed for ${component} ***"
 }
