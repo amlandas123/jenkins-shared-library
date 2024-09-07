95dadffa775b747655d5097a4c367741f8486973
Amlan Das <dasamlan271988@gmail.com>
2024-09-07 21:51:35 +0530

    jkshared

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -3,17 +3,17 @@ def lintchecks(){
         sh "echo *** Lint check starting for ${component} ***"
         sh "npm i jslint"
         sh "/home/centos/node_modules/jslint/bin/jslint.js server.js || true"
-        sh "echo *** Lint check completed ***"
+        sh "echo *** Lint check completed for ${component} ***"
 }
 
-def call('component'){
+def call(){
         pipeline{
                 agent any
                 stages{
                     stage("lintchecks"){
                         steps{
                                 script {
-                                     lintchecks('component')           
+                                     lintchecks()           
                 }
             }
         }
