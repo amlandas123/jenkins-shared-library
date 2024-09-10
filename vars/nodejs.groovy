9f459b1977055734e45245c0e0c9aa510e5e9215
Amlan Das <dasamlan271988@gmail.com>
2024-09-10 15:19:22 +0530

    jkshared

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -5,6 +5,13 @@ def lintchecks(){
         sh "node_modules/jslint/bin/jslint.js server.js || true"
         sh "echo *** Lint check completed for ${component} ***"
 }
+def sonarchecks(){
+        sh '''
+        sonar-scanner -Dsonar.host.url=http://172.31.33.165:9000 -Dsonar.sources=. -Dsonar.projectKey=${component} -Dsonar.login=admin -Dsonar.password=password
+
+        '''
+}
+
 
 def call(){
         pipeline{
@@ -22,7 +29,9 @@ def call(){
                         }
                     stage("Static code analysis"){
                         steps{
-                                sh "echo static code analysis in process"
+                                script {
+                                        sonarchecks()
+                                }
                         }
                         
                     }    
