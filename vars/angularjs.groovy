7ed00734426763a81dfc901bf66f9bd1aaceddb6
Amlan Das <dasamlan271988@gmail.com>
2024-09-12 10:59:43 +0530

    jkshared

---
+++ vars/angularjs.groovy
@@ -0,0 +1,50 @@
+def lintchecks(){
+        sh "whoami"
+        sh "echo *** Lint check starting for ${component} ***"
+        sh "npm install jslint"
+        sh "node_modules/jslint/bin/jslint.js server.js || true"
+        sh "echo *** Lint check completed for ${component} ***"
+}
+
+
+
+def call(){
+        pipeline{
+                agent{
+                        label "node"
+                }
+                stages{
+                    stage("lintchecks"){
+                        steps{
+                                script {
+                                     lintchecks()           
+                                       }
+                                }
+                
+                        }
+                    stage("Static code analysis"){
+                        steps{
+                                script {
+                                        env.ARGS='-Dsonar.sources=.'
+                                        common.sonarchecks()
+                                }
+                        }
+                        
+                    }
+                    stage("get the sonar result"){
+                        steps{
+                                sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > gates.sh"
+                                sh "sudo bash gates.sh admin password ${sonar_url} ${component}"
+                        }
+                    }
+                    stage("unit code testing"){
+                        steps{
+                                sh "echo Testing is in progress"
+                        }
+                        
+                    }        
+                }
+
+
+}
+}               
